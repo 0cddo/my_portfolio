749259b00f7859cb6c5961dbeaa34d8417febbee
0cddo <0.c.dddo@gmail.com>
2021-10-08 15:20:40 +0900

    make about section

--- js/script.js
+++ js/script.js
@@ -5,4 +5,13 @@ $(document).ready(function () {
     play: 3000,
     pagination: false,
   });
+
+  var typed = new Typed(".typed", {
+    strings: ["Soft Engineer.", "Web Debeloper.", "Front-end Developer."],
+    typeSpeed: 80,
+    loop: true,
+    startDelay: 1000,
+    backDelay: 600,
+    showCursor: false,
+  });
 });
